66c00f7d7d356e923832ac9c44af17eb115166cf
Fitsum Mesfin <58865020+fitsumM12@users.noreply.github.com>
2025-10-14 16:25:13 +0300

    ✅✅✅Updated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types";
 export * from "./convert";
 export * from "./format";
+export * from "./EthiopianDatePicker"
 export { monthNames, weekdayNames } from "./i18n";
 export { isGregorianLeap, isEthiopianLeap } from "./utils";
